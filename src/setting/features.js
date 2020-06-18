2acdffd87e9fa2885cc488b29c9293830045b4b1
nsfe <nsfe0619@gmail.com>
2020-06-18 12:16:55 +0800

    bug fix

--- src/setting/features.js
+++ src/setting/features.js
@@ -5,7 +5,7 @@ module.exports.features = features;
 function features() {
   return featuresArray;
 }
-let featuresArray = [
+const featuresArray = [
   {
     keyword: "你會做什麼",
     type: "equal",
@@ -29,7 +29,7 @@ let featuresArray = [
   },
 ];
 function queryFeatures(context) {
-  const message = "";
+  let message = "";
   featuresArray.forEach((element) => {
     message += message ? "、" : "";
     message += element.keyword;
