bea564a33987c5eb2006f3570f854b1886a8a62b
nsfe <nsfe0619@gmail.com>
2020-06-18 12:12:37 +0800

    no message

--- src/setting/features.js
+++ src/setting/features.js
@@ -3,20 +3,37 @@ var divination = require("../func/divination");
 module.exports.features = features;
 
 function features() {
-  return [
-    {
-      keyword: "看妹子",
-      type: "equal",
-      function: function (context) {
-        return findBeauty.findBeauty(context);
-      },
+  return featuresArray;
+}
+let featuresArray = [
+  {
+    keyword: "你會做什麼",
+    type: "equal",
+    function: function (context) {
+      return queryFeatures(context);
     },
-    {
-      keyword: "運勢",
-      type: "indexOf",
-      function: function (context) {
-        return divination.divination(context);
-      },
+  },
+  {
+    keyword: "看妹子",
+    type: "equal",
+    function: function (context) {
+      return findBeauty.findBeauty(context);
     },
-  ];
+  },
+  {
+    keyword: "運勢",
+    type: "indexOf",
+    function: function (context) {
+      return divination.divination(context);
+    },
+  },
+];
+function queryFeatures(context) {
+  const message = "";
+  featuresArray.forEach((element) => {
+    message += message ? "、" : "";
+    message += element.keyword;
+  });
+
+  context.sendText("我會做這些事情喔(.w. ) %0D%0A" + message);
 }
